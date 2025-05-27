7587802c44db0e38fb25254899d846780089ee1b
mantheys <sergio.manthey@gmail.com>
2025-05-28 01:26:35 +0200

    UPLOAD SETUP SCRIPTS

---
+++ dune.sh
@@ -0,0 +1,15 @@
+# Check if dune_plot_style is already installed by looking for the directory named "dune_plot_style"
+if [ -d "dune_plot_style" ]; then
+    echo "dune_plot_style is already installed."
+else
+    echo "dune_plot_style not found, running dune.sh to install it." 
+    export DUNE_PLOT_STYLE_LATEST_TAG=`curl --silent "https://api.github.com/repos/DUNE/dune_plot_style/releases" | jq -r 'map(select(.prerelease == false)) | first | .tag_name'`
+    wget --no-check-certificate https://github.com/DUNE/dune_plot_style/archive/refs/tags/${DUNE_PLOT_STYLE_LATEST_TAG}.tar.gz -O dune_plot_style.tar.gz
+    # Extract the tar.gz file and make sure to rename the directory to "dune_plot_style"
+    tar -xvzf dune_plot_style.tar.gz
+    export DUNE_PLOT_STYLE_VERSION=`echo ${DUNE_PLOT_STYLE_LATEST_TAG} | sed 's/^v//'`
+    mv dune_plot_style-${DUNE_PLOT_STYLE_VERSION} dune_plot_style
+    cd dune_plot_style
+    # Install the dune_plot_style package
+    python3 -m pip install .
+fi
--- venv.sh
+++ venv.sh
@@ -1,1 +1,12 @@
-source .venv/bin/activate
+# Check if .venv file already exists
+if [ -d ".venv" ]; then
+    echo "Virtual environment already exists."
+    source .venv/bin/activate
+else
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+    source .venv/bin/activate
+    echo "Installing dependencies..."
+    pip install -r requirements.txt
+    echo "Virtual environment created and dependencies installed." 
+fi
