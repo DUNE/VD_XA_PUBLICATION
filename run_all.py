ee94d3503807b6990a5e7f035d4bc21c1aef6938
mantheys <sergio.manthey@gmail.com>
2025-05-28 01:33:57 +0200

    UPLOAD SCRIPT TO RUN ALL PLOTS

---
+++ run_all.py
@@ -0,0 +1,15 @@
+# This is a script to run all the scripts
+import os
+def run_script(script_name):
+    print(f"Running {script_name}...")
+    os.system(f"python {script_name}")
+if __name__ == "__main__":
+    scripts = [
+        "result_combined.py",
+        "result_comparison.py"
+    ]
+    
+    for script in scripts:
+        run_script(script)
+    
+    print("All scripts executed successfully.")
